4ffb532c1b4854912121f53eefbf3aff0492fccc
Asa Grace <asag182@gmail.com>
2021-06-26 19:05:06 -0600

    questions

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('../Professional-README-Generator/Develop/utils/generateMarkdown')
+const generateMarkdown = require('../Professional-README-Generator/Develop/utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =  () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What do you want to write as your title',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'What would you like to write as your description',
+        },
+        {
+            type: 'input',
+            name: 'Table of Contents',
+            message: ''
+        },
+        {
+            type: 'input',
+            name: 'Questions',
+            message: ''
+        }
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
