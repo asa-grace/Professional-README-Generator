59f7f77d7bfc29ace751c2d378f6ef8986ff9016
Asa Grace <asag182@gmail.com>
2021-06-27 23:09:30 -0600

    added prompts for information

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // TODO: Include packages needed for this application
-const { fstat } = require('fs');
+const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('../Professional-README-Generator/Develop/utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions =  () => {
+const questions = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'title',
-            message: 'What do you want to write as your title',
+            message: 'What is the projects title?',
             validate: titleInput => {
                 if (titleInput) {
                     return true;
@@ -19,38 +18,68 @@ const questions =  () => {
         },
         {
             type: 'input',
-            name: 'Description',
-            message: 'What would you like to write as your description',
+            name: 'description',
+            message: 'Provide a description for your project',
         },
         {
             type: 'input',
-            name: 'Table of Contents',
-            message: ''
+            name: 'installation',
+            message: 'What are the steps required to install your project?'
         },
         {
             type: 'input',
-            name: 'Questions',
-            message: ''
-        }
+            name: 'usage',
+            message: 'Please provide instructions and examples of how to use your application.'
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Please list any contributors with their github username, or other sources used to make your application'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What is your GitHub Username?'
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter a link to your GitHub or GitHub Repository.'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your Email'
+        },
+        // {
+        //     type: 'checkbox',
+        //     name: 'license',
+        //     message: 'Select the license your application is covered under'
+        // }
     ]);
+};
+
+questions();
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('', fileContent, err => {
-            if (err);
-            return;
-        })
+// function writeToFile(README, data) {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile('READ-ME.md', fileContent, err => {
+//             if (err) {
+//                 reject(err);
+//                 return;
+//             }
         
-        resolve({
-            ok: true,
-            message: 'File created!'
-        })
-    
-}
+//             resolve({
+//                 ok: true,
+//                 message: 'File created!'
+//             }); 
+//         });
+//     }); 
+// }
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
